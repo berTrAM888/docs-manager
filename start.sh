f61a9f8fd7d3dfbcb577361d0fab40d726d49637
berTrAM888 <928921254@qq.com>
2019-08-14 15:15:36 +0000

    add

--- start.sh
+++ start.sh
@@ -4,3 +4,5 @@ export LANG=C.UTF-8
 nohup mkdocs serve -a 127.0.0.1:8081 >> /root/docs-manager/logs/font.log &
 cd /root/docs-manager/backend/
 nohup python3 start.py >> /root/docs-manager/logs/back.log &
+service nginx restart
+tail -f /root/docs-manager/logs/font.log
