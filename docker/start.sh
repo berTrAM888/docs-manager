bce447d75eaee1bd342800de5fb775171995c751
berTrAM888 <928921254@qq.com>
2019-08-14 16:17:10 +0000

    add dockerfile

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author:berTrAM
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+cd /root/docs-manager/backend/
+nohup python3 start.py >> /root/docs-manager/logs/back.log &
+service nginx restart
+cd ..
+nohup mkdocs serve -a 127.0.0.1:8081 >> /root/docs-manager/logs/font.log &
+service nginx restart
+tail -f /root/docs-manager/logs/font.log
